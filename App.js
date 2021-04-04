b59f2f7b2b83bd0918cc726a8ede544ac11d187f
FelipeMoraisOC <felipemoraisoc@outlook.com>
2021-04-04 00:38:27 -0300

    2 Bugs fixed. First, comma was not appearing after clicking any operations Button; Second, comma was sometimes converted to number, fixed using ${}.

--- App.js
+++ App.js
@@ -20,14 +20,14 @@ export default class App extends Component {
 
   addDigit = n => {
     
+    //Clears display. Prevent displayValue being 0'n' and adds to the side, the next value in display
+    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay 
+
      //prevents more than one '.' being displayed
-    if(n === '.' && this.state.displayValue.includes('.')){
+     if(n === '.'&& !clearDisplay && this.state.displayValue.includes('.')){
       return
     }
 
-    //Clears display. Prevent displayValue being 0'n' and adds to the side, the next value in display
-
-    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay 
     const currentValue = clearDisplay ? '' : this.state.displayValue
     const displayValue = currentValue + n 
     this.setState({ displayValue, clearDisplay: false })
@@ -45,11 +45,31 @@ export default class App extends Component {
   }
 
   clearMemory = () => {
-    this.setState({ ...initialState})
+    this.setState({ ...initialState })
   }
 
-  setOperation = () => { //5 botoes de operacao
-    
+  //Does math operations using eval / * + -
+  setOperation = operation => { 
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true})
+    } else {
+      const equals = operation === '='
+      const values = [ ...this.state.values]
+      try {
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+      } catch (e) {
+          values[0] = this.state.values[0]
+      }
+
+      values[1] = 0
+      this.setState({
+        displayValue: `${values[0]}`,
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: true,
+        values,
+      })
+    }
   }
 
   render() {
@@ -58,22 +78,22 @@ export default class App extends Component {
         <Display value={this.state.displayValue} />
         <View style={styles.buttons}>
           <Button label='AC'  triple onClick={this.clearMemory}/>
-          <Button label='/'  operation onClick={() => this.setOperation('/')}/>
+          <Button label='/'  operation onClick={ this.setOperation }/>
           <Button label='7' onClick={this.addDigit} />
           <Button label='8' onClick={this.addDigit} />
           <Button label='9' onClick={this.addDigit} />
-          <Button label='*' operation onClick={() => this.setOperation('/')}/>
+          <Button label='*' operation onClick={ this.setOperation }/>
           <Button label='4' onClick={this.addDigit} />
           <Button label='5' onClick={this.addDigit} />
           <Button label='6' onClick={this.addDigit} />
-          <Button label='-' operation onClick={() => this.setOperation('-')}/>
+          <Button label='-' operation onClick={ this.setOperation }/>
           <Button label='1' onClick={this.addDigit}/>
           <Button label='2' onClick={this.addDigit}/>
           <Button label='3' onClick={this.addDigit}/>
-          <Button label='+' operation onClick={() => this.setOperation('+')}/>
+          <Button label='+' operation onClick={ this.setOperation }/>
           <Button label='0' double onClick={this.addDigit}/>
           <Button label='.' onClick={this.addDigit}/>
-          <Button label='=' operation onClick={() => this.setOperation('+')}/>
+          <Button label='=' operation onClick={ this.setOperation }/>
         </View>
        
       </View> 
